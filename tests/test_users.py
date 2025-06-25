446be080ef7fc0ae07b6516046959ac63affccfa
Zainab <zainabsameer197@gmail.com>
2025-06-25 23:56:30 +0300

    test_users with 3 warning

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,38 +1,41 @@
-import pytest
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/.."))) 
+
+from main import app
 from fastapi.testclient import TestClient
-from main import app  # ✅ correct
+
 client = TestClient(app)
 
+
 def test_register_user_success():
-    response = client.post("/register", json={
-        "username": "zainab_test_1",
-        "email": "zainab_test_1@example.com",
+    response = client.post("/api/register", json={
+        "username": "zainab_test1",
+        "email": "zainab_test1@kpmg.com",
         "password": "strongpassword123",
         "is_admin": False,
-        "created_at": "2025-06-24T20:00:00"
+        "created_at": "2025-06-25T21:00:00"
     })
     assert response.status_code == 200
     data = response.json()
-    assert data["username"] == "zainab_test_1"
-    assert data["email"] == "zainab_test_1@example.com"
+    assert data["username"] == "zainab_test1"
+    assert data["email"] == "zainab_test1@kpmg.com"
 
-def test_register_existing_user():
-    # First registration
-    client.post("/register", json={
-        "username": "zainab_existing",
-        "email": "zainab_existing@example.com",
-        "password": "mypass",
+def test_register_user_duplicate():
+    client.post("/api/register", json={
+        "username": "zainab_d",
+        "email": "zainab_d@kpmg.com",
+        "password": "password456",
         "is_admin": False,
-        "created_at": "2025-06-24T20:00:00"
+        "created_at": "2025-06-25T21:00:00"
     })
 
-    # Attempt to re-register same username/email
-    response = client.post("/register", json={
-        "username": "zainab",
-        "email": "zainab@kpmg.com",
-        "password": "123456",
+    response = client.post("/api/register", json={
+        "username": "zainab_d",
+        "email": "zainab_d@kpmg.com",
+        "password": "password456",
         "is_admin": False,
-        "created_at": "2025-06-24T20:00:00"
+        "created_at": "2025-06-25T21:00:00"
     })
     assert response.status_code == 400
     assert response.json()["detail"] == "Username or email already exists"
